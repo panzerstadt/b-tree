e0340f526a38cf0302765ff34317c4c4432dbba4
ts-liqun.tang <ts-liqun.tang@rakuten.com>
2021-05-31 15:17:36 +0900

    todo: remove a value in a btree

--- README.md
+++ README.md
@@ -16,6 +16,25 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 - 4 basic operations in a BTreeNode
 - all of these functions keep the array of references of values as small as possible using array.splice() - array mutation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 
+### how to decide what order of btree
+
+- when instantiating a btree, we need to decide what order (max num of keys per non-leaf child node).
+- to pick one, we have to see where the btree lives on the storage device.
+- the order picked is so that the resulting node is as large as possible while still fitting into the "block device page size"
+- common block sizes for HDD (spinning disks) are 512 bytes, 1kb or 4kb
+- for SSDs, a block size is 4KB in physical size (512KB logical size) - https://superuser.com/questions/976257/page-sizes-ssd
+
+> Typically, you'd choose the order so that the resulting node is as large as possible while still fitting into the block device page size. If you're trying to build a B-tree for an on-disk database, you'd probably pick the order such that each node fits into a single disk page, thereby minimizing the number of disk reads and writes necessary to perform each operation. If you wanted to build an in-memory B-tree, you'd likely pick either the L2 or L3 cache line sizes as your target and try to fit as many keys as possible into a node without exceeding that size. In either case, you'd have to look up the specs to determine what size to use.
+
+#### refs
+
+- how to decide btree order: https://stackoverflow.com/questions/28677734/how-to-decide-order-of-b-tree
+- block sizing in hdd vs ssd: https://superuser.com/questions/976271/ssd-block-sizing#:~:text=The%20smallest%20unit%20of%20an,a%20block%20contains%20512%20KB.
+- logical vs physical sizes: http://grandperspectiv.sourceforge.net/HelpDocumentation/FileSizeMeasures.html#:~:text=Logical%20size%2C%20which%20corresponds%20to,the%20information%20in%20the%20file.&text=Physical%20size%2C%20which%20represents%20the,the%20way%20files%20are%20stored.
+- ref: http://books.gigatux.nl/mirror/kerneldevelopment/0672327201/ch13lev1sec1.html#:~:text=The%20kernel%20also%20requires%20that,1%20kilobyte%2C%20and%204%20kilobytes.
+- holy heckums ssds have different btrees: https://www.researchgate.net/publication/293476436_AS_B-tree_A_Study_of_an_Efficient_B-tree_for_SSDs
+- interesting reads on how ssds change what is the most optimal way to store and retrieve data: https://softwareengineering.stackexchange.com/questions/114923/will-b-trees-and-other-data-structures-become-obsolete-with-the-advent-of-solid
+
 ## Getting Started
 
 First, run the development server:
--- components/BTree/btree.ts
+++ components/BTree/btree.ts
@@ -1,5 +1,8 @@
 import { BTreeNode } from "./node";
 
+/**
+ * for order, pick a general size targeting cpu l2/l3 caches
+ */
 export class BTree {
   root: BTreeNode;
   order: number;
@@ -11,9 +14,32 @@ export class BTree {
 
   /**
    * search a value in the Tree and return the Node O(log N)
-   * @param value
+   * @param node the BTreeNode
+   * @param value value stored in node (number)
+   *
+   * how it works:
+   * - traverse the search tree from top to bottom looking for the correct child in every iteration
+   * - in the BST the algo goes right if target `k` is greater than the actual node value, and left if lower.
+   * - then using the same logic, the BTree continues to the subtree where it target `k` should be stored
+   * - this is done by comparing it with the values stored in the actual node.
    */
-  searchValue(value: number) {}
+  searchValue(node: BTreeNode, value: number) {
+    if (node.values.includes(value)) {
+      console.info("found the value!");
+      return node;
+    }
+
+    if (node.leaf) {
+      console.warn("value was not found.");
+      return null;
+    }
+
+    let child = 0;
+    while (child < node.n && node.values[child] < value) {
+      child++;
+    }
+    return this.searchValue(node.children[child], value);
+  }
 
   /**
    * delete the value from the tree O(log N)
@@ -25,6 +51,13 @@ export class BTree {
    * delete a value from a node
    * @param node
    * @param value
+   *
+   * how this works:
+   * - recursively going through the height of the tree,
+   * - trying to move the target value k to the bottom
+   * - but always being sure that the next node has more than t - 1
+   *   values before visiting it, otherwise do some operations before
+   *   moving down
    */
   deleteFromNode(node: BTreeNode, value: number) {}
 
@@ -46,14 +79,51 @@ export class BTree {
    * insert a new value in the tree O(log N)
    * @param value
    */
-  insert(value) {}
+  insert(value) {
+    const actual = this.root;
+    if (actual.n === 2 * this.order - 1) {
+      // if full
+      const temp = new BTreeNode(false);
+      temp.tree = this;
+      this.root = temp;
+      temp.addChild(actual, 0);
+      this.split(actual, temp, 1);
+      this.insertNonFull(temp, value);
+    } else {
+      this.insertNonFull(actual, value);
+    }
+  }
 
   /**
    * insert a value in a not-full node O(1)
    * @param node
    * @param value
    */
-  insertNonFull(node: BTreeNode, value: number) {}
+  insertNonFull(node: BTreeNode, value: number) {
+    if (node.leaf) {
+      node.addValue(value); // insert
+      return;
+    }
+
+    let temp = node.n;
+    while (temp >= 1 && value < node.values[temp - 1]) {
+      // iterate backward on node length
+      // while counter is still more than 1 and
+      // and value is smaller than the left value
+      temp--;
+    }
+
+    if (node.children[temp].n === 2 * this.order - 1) {
+      // if the current node is a full node (2t -1)
+      // we split the node
+      this.split(node.children[temp], node, temp + 1);
+      if (value > node.values[temp]) {
+        temp++;
+      }
+    }
+
+    this.insertNonFull(node.children[temp], value);
+  }
 
   /**
    * divide child node from parent into parent.values[pos-1] and parent.value[pos]
@@ -61,5 +131,20 @@ export class BTree {
    * @param parent
    * @param pos
    */
-  split(child: BTreeNode, parent: BTreeNode, pos: number) {}
+  split(child: BTreeNode, parent: BTreeNode, pos: number) {
+    const newChild = new BTreeNode(child.leaf);
+    newChild.tree = this.root.tree; // ?
+    for (let k = 1; k < this.order; k++) {
+      newChild.addValue(child.removeValue(this.order));
+    }
+    if (!child.leaf) {
+      for (let k = 1; k <= this.order; k++) {
+        newChild.addChild(child.removeChild(this.order), k - 1);
+      }
+    }
+    parent.addChild(newChild, pos);
+    // pass value to parent
+    parent.addValue(child.removeValue(this.order - 1));
+    parent.leaf = false;
+  }
 }
